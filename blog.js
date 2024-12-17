68bd5e3fdcfd18848f202f1fa9fe40a83ded3ca5
Baba Faye <fayebaba81@gmail.com>
2024-12-17 02:13:35 +0000

    le bout du tunnel

--- blog.js
+++ blog.js
@@ -1,12 +1,5 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     recupererArticle();
-// });
-
-
 // recupwration de mes donnes sur le dom
-
 const pagePrincipale = document.querySelector(".liste");
-
 const formulaire = document.querySelector(".form");
 
 // recuperation des elements du formulaire
@@ -17,7 +10,6 @@ const myForm = document.getElementById("formulaire");
 
 // form modifier
 const formulaireModifier = document.querySelector("#formeModifier");
-console.log(formulaireModifier);
 
 const titreModif = document.querySelector("#titleModif");
 const contenuModif = document.querySelector("#contentModif");
@@ -42,7 +34,6 @@ AjouterBlog.addEventListener("click", () => {
     
 })
 
-
 // ecouter le l'envoie du formulaire
 formulaire.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -57,11 +48,21 @@ formulaire.addEventListener("submit", (e) => {
 
 // sauvegarder article
 function sauvgarderArticle(titr, cont, aut) {
+
+    const date = new Date().toLocaleDateString('fr-FR', {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric'
+
+    })
     let donnes = {
         id: Date.now(),
        titre: titr,
        contenu: cont,
-       auteur: aut
+       auteur: aut,
+       date: date
     }
  
     let save = JSON.parse(localStorage.getItem('article')) || []
@@ -81,14 +82,12 @@ function sauvgarderArticle(titr, cont, aut) {
      }
  
      save.forEach(element => {
-         ajouterArticle(element.titr, element.cont, element.aut)
+         ajouterArticle(element.titre, element.contenu, element.auteur,element.id,element.date)
      })
  }
 
 // fonction pour ajouter un article
-function ajouterArticle(titr, cont, aut) {
-console.log('titr', titr, 'cont', cont, 'aut', aut);
-// creation d'element d'affichage
+function ajouterArticle(titr, cont, aut,id,date) {
 const items_content = document.createElement('div')
 listeTask.appendChild(items_content)
 items_content.classList.add('items') //div conteneur
@@ -99,9 +98,8 @@ items_content.appendChild(itemText)
 const item_titre = document.createElement('input')
 item_titre.type = 'text'
 item_titre.classList.add('text')
-item_titre.value = titr
+item_titre.value =`TITRE:  ${titr}` 
 item_titre.setAttribute('readonly', 'readonly')
-// console.log(item_titre.value);
 itemText.appendChild(item_titre)
 
 const edit_titre = document.createElement('button')
@@ -120,17 +118,21 @@ edit_titre.addEventListener('click', (e)=>{
     }else{
         edit_titre.innerHTML = 'Edit'
         item_titre.setAttribute('readonly', 'readonly')
-        misAJour(titr, cont, aut)
+        misAJour(id,"titre", item_titre.value)
+
+        console.log(item_titre.value);
+        
     }
 })
 
 // partie contenu
 const itemTextContenu = document.createElement('li')//li  contenu
 items_content.appendChild(itemTextContenu)
-const item_Contenu = document.createElement('input')
+const item_Contenu = document.createElement('textarea')
 item_Contenu.type = 'text'
+item_Contenu.rows = '10'
 item_Contenu.classList.add('text')
-item_Contenu.value = cont
+item_Contenu.value = `CONTENU : ${cont}`
 item_Contenu.setAttribute('readonly', 'readonly')
 itemTextContenu.appendChild(item_Contenu)
 
@@ -150,7 +152,8 @@ edit_contenu.addEventListener('click', (e)=>{
     }else{
         edit_contenu.innerHTML = 'Edit'
         item_Contenu.setAttribute('readonly', 'readonly')
-        misAJour(titr, cont, aut)
+        misAJour(id,"contenu",item_Contenu.value)
+        console.log(item_Contenu.value)
     }
 })
 
@@ -160,7 +163,7 @@ items_content.appendChild(itemTextAuteur)
 const item_Auteur = document.createElement('input')
 item_Auteur.type = 'text'
 item_Auteur.classList.add('text')
-item_Auteur.value = aut
+item_Auteur.value = `AUTEUR : ${aut}` 
 item_Auteur.setAttribute('readonly', 'readonly')
 itemTextAuteur.appendChild(item_Auteur)
 
@@ -180,31 +183,23 @@ edit_Auteur.addEventListener('click', (e)=>{
     }else{
         edit_Auteur.innerHTML = 'Edit'
         item_Auteur.setAttribute('readonly', 'readonly')
-        misAJour(id,titr, cont, aut)
+        misAJour(id,"auteur", item_Auteur.value)
+
+       console.log(item_Auteur.value);
+        
     }
 })
-
-// affichage date de publication
-const date = new Date()
-const affichage = date.toLocaleDateString('fr-FR',{
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-})
-console.log(affichage);
 
 const itemTextDate = document.createElement('li')//li  date
 items_content.appendChild(itemTextDate)
 const item_date = document.createElement('input')
 item_date.type = 'text'
 item_date.classList.add('text')
-item_date.value = affichage
+item_date.value = `DATE PUBLICATION: ${date}`
 item_date.setAttribute('readonly', 'readonly')
 itemTextDate.appendChild(item_date)
 
+// button supprimer
 const supp = document.createElement('button')
 supp.type = 'button'
 supp.classList.add('supp')
@@ -214,35 +209,34 @@ itemTextDate.appendChild(supp)
 
 supp.addEventListener('click', (e)=>{
     items_content.remove()
-    supprimerArticle(titr)
+    supprimerArticle(id)
 })
 }
 
-// fonction modifier
-function misAJour(id,newTitre, newContenu, newAuteur){
+// fonction mis a jour
+function misAJour(id,champ, newValue){
     let save = JSON.parse(localStorage.getItem('article')) || []
-    save = save.map(article => {
-        if (article.id === id) {
+    
+    save = save.map(article=> {
+
+        if(article.id === id){
             return {
-                ...article, 
-                titre: newTitre,
-                contenu: newContenu,
-                auteur: newAuteur
-            };
+              ...article,
+              [champ]: newValue,  //mettre a jour un champ specifique
+            }
+           
         }
-        return article;
-    });
+
+        return article
+    })
+    localStorage.setItem('article', JSON.stringify(save))
 
     }
    
 // fonction supprimer
-function supprimerArticle(eleSupp){
+function supprimerArticle(id){
     let save = JSON.parse(localStorage.getItem('article')) || []
-    save.forEach((element, index) => {
-        if(element.titr == eleSupp){
-            save.splice(index, 1)
-        }
-    });
+    save = save.filter(article => article.id !==id);
     localStorage.setItem('article', JSON.stringify(save))
    
 }
--- essai.js
+++ essai.js
@@ -1,66 +1,149 @@
-document.addEventListener('DOMContentLoaded', () => {
-    recupererArticle();
-});
+// Sauvegarder article
+function sauvgarderArticle(titr, cont, aut) {
+    const date = new Date().toLocaleDateString('fr-FR', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+    });
 
-AjouterBlog.addEventListener("click", () => {
-    formulaire.style.display = "block";
-    pagePrincipale.style.display = "none";
-    myForm.style.display = "block";
-});
+    let donnes = {
+        id: Date.now(),
+        titre: titr,
+        contenu: cont,
+        auteur: aut,
+        date: date // Enregistrer la date de création
+    };
 
-formulaire.addEventListener("submit", (e) => {
-    e.preventDefault();
-    formulaire.style.display = "none";
-    pagePrincipale.style.display = "block";
-
-    ajouterArticle(titre.value, contenu.value, auteur.value);
-    sauvgarderArticle(titre.value, contenu.value, auteur.value);
-
-    myForm.reset();
-});
-
-function sauvgarderArticle(titre, contenu, auteur) {
-    let donnes = { titre, contenu, auteur };
     let save = JSON.parse(localStorage.getItem('article')) || [];
     save.push(donnes);
     localStorage.setItem('article', JSON.stringify(save));
 }
 
+// Récupérer les articles sauvegardés
 function recupererArticle() {
     let save = JSON.parse(localStorage.getItem('article')) || [];
-    save.forEach(element => {
-        ajouterArticle(element.titre, element.contenu, element.auteur);
+
+    save.forEach(article => {
+        ajouterArticle(article.titre, article.contenu, article.auteur, article.id, article.date);
     });
 }
 
-function ajouterArticle(titre, contenu, auteur) {
+// Ajouter un article
+function ajouterArticle(titr, cont, aut, id, date) {
     const items_content = document.createElement('div');
+    listeTask.appendChild(items_content);
     items_content.classList.add('items');
 
+    // Partie titre
+    const itemText = document.createElement('li');
+    items_content.appendChild(itemText);
     const item_titre = document.createElement('input');
     item_titre.type = 'text';
-    item_titre.value = titre;
+    item_titre.classList.add('text');
+    item_titre.value = titr;
     item_titre.setAttribute('readonly', 'readonly');
-    items_content.appendChild(item_titre);
+    itemText.appendChild(item_titre);
 
-    const item_contenu = document.createElement('input');
-    item_contenu.type = 'text';
-    item_contenu.value = contenu;
-    item_contenu.setAttribute('readonly', 'readonly');
-    items_content.appendChild(item_contenu);
+    const edit_titre = document.createElement('button');
+    edit_titre.type = 'button';
+    edit_titre.classList.add('edit');
+    edit_titre.style.backgroundColor = 'green';
+    edit_titre.innerHTML = 'Edit';
+    itemText.appendChild(edit_titre);
 
-    const item_auteur = document.createElement('input');
-    item_auteur.type = 'text';
-    item_auteur.value = auteur;
-    item_auteur.setAttribute('readonly', 'readonly');
-    items_content.appendChild(item_auteur);
+    edit_titre.addEventListener('click', () => {
+        if (edit_titre.innerHTML.toLowerCase() === 'edit') {
+            edit_titre.innerHTML = 'Save';
+            item_titre.removeAttribute('readonly');
+            item_titre.focus();
+        } else {
+            edit_titre.innerHTML = 'Edit';
+            item_titre.setAttribute('readonly', 'readonly');
+            misAJour(id, "titre", item_titre.value);
+        }
+    });
 
+    // Partie contenu
+    const itemTextContenu = document.createElement('li');
+    items_content.appendChild(itemTextContenu);
+    const item_Contenu = document.createElement('input');
+    item_Contenu.type = 'text';
+    item_Contenu.classList.add('text');
+    item_Contenu.value = cont;
+    item_Contenu.setAttribute('readonly', 'readonly');
+    itemTextContenu.appendChild(item_Contenu);
+
+    const edit_contenu = document.createElement('button');
+    edit_contenu.type = 'button';
+    edit_contenu.classList.add('edit');
+    edit_contenu.style.backgroundColor = 'green';
+    edit_contenu.innerHTML = 'Edit';
+    itemTextContenu.appendChild(edit_contenu);
+
+    edit_contenu.addEventListener('click', () => {
+        if (edit_contenu.innerHTML.toLowerCase() === 'edit') {
+            edit_contenu.innerHTML = 'Save';
+            item_Contenu.removeAttribute('readonly');
+            item_Contenu.focus();
+        } else {
+            edit_contenu.innerHTML = 'Edit';
+            item_Contenu.setAttribute('readonly', 'readonly');
+            misAJour(id, "contenu", item_Contenu.value);
+        }
+    });
+
+    // Partie auteur
+    const itemTextAuteur = document.createElement('li');
+    items_content.appendChild(itemTextAuteur);
+    const item_Auteur = document.createElement('input');
+    item_Auteur.type = 'text';
+    item_Auteur.classList.add('text');
+    item_Auteur.value = aut;
+    item_Auteur.setAttribute('readonly', 'readonly');
+    itemTextAuteur.appendChild(item_Auteur);
+
+    const edit_Auteur = document.createElement('button');
+    edit_Auteur.type = 'button';
+    edit_Auteur.classList.add('edit');
+    edit_Auteur.style.backgroundColor = 'green';
+    edit_Auteur.innerHTML = 'Edit';
+    itemTextAuteur.appendChild(edit_Auteur);
+
+    edit_Auteur.addEventListener('click', () => {
+        if (edit_Auteur.innerHTML.toLowerCase() === 'edit') {
+            edit_Auteur.innerHTML = 'Save';
+            item_Auteur.removeAttribute('readonly');
+            item_Auteur.focus();
+        } else {
+            edit_Auteur.innerHTML = 'Edit';
+            item_Auteur.setAttribute('readonly', 'readonly');
+            misAJour(id, "auteur", item_Auteur.value);
+        }
+    });
+
+    // Date de création
+    const itemTextDate = document.createElement('li');
+    items_content.appendChild(itemTextDate);
+    const item_date = document.createElement('input');
+    item_date.type = 'text';
+    item_date.classList.add('text');
+    item_date.value = date;
+    item_date.setAttribute('readonly', 'readonly');
+    itemTextDate.appendChild(item_date);
+
+    // Bouton supprimer
     const supp = document.createElement('button');
+    supp.type = 'button';
+    supp.classList.add('supp');
+    supp.style.backgroundColor = 'red';
     supp.innerHTML = 'Supprimer';
+    itemTextDate.appendChild(supp);
+
     supp.addEventListener('click', () => {
         items_content.remove();
+        supprimerArticle(id);
     });
-    items_content.appendChild(supp);
-
-    listeTask.appendChild(items_content);
 }
